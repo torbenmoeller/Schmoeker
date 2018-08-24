80ff223f3f584f7d410ed58bd58d5873a3b2ec69
Torben Möller <torben.moeller@gmx.net>
2018-08-25 00:36:20 +0200

    fix: removed notifications if app is active

--- app/src/main/java/com/schmoeker/sync/MyJobService.java
+++ app/src/main/java/com/schmoeker/sync/MyJobService.java
@@ -4,11 +4,13 @@ import android.content.Intent;
 
 import com.firebase.jobdispatcher.JobParameters;
 import com.firebase.jobdispatcher.JobService;
+import com.schmoeker.KEYS;
 
 public class MyJobService extends JobService {
     @Override
     public boolean onStartJob(JobParameters job) {
         Intent service = new Intent(getApplicationContext(), SyncService.class);
+        service.putExtra(KEYS.AUTOUPDATE, true);
         getApplicationContext().startService(service);
         return false;
     }
